2094efe04e5de8aa3c0d0f1357f3ff76c2b00f2b
Maria Jose Tamayo <mariajose.tamayo@gmail.com>
2018-06-18 15:33:06 -0600

    My first commit ever

---
+++ app.js
@@ -0,0 +1,12 @@
+const kelvin = 0;
+//kelvin will stay constant
+let celsius = kelvin - 273;
+//celsius is kelvin menos 273
+let farenheit = celsius *(9/5) + 32;
+//farenheit is celsius multiplied by 9/5 plus 32
+farenheit = Math.floor(farenheit);
+//farenheit is a decimal number
+console.log(`The temperature is ${farenheit} degrees Farenheit`);
+newton = celsius *(33/100);
+newton = Math.floor(newton);
+console.log(`${celsius} celsius are ${newton} newtons.`);
